224ec9d5156c6fdc9200a3177ad0b5e9c18d495e
Matt Casperson <mc.casperson@gmail.com>
2021-10-28 11:32:49 -0700

    add UI and functionality to accept multiple number input
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- README.md
+++ README.md
@@ -62,25 +62,10 @@ Test: When we input a number between 1-3999 it outputs the correct symbols
 Code: romanNumeral("2021");
 Expected Output: "MMXXI"
 
+Test: When we input more than one number it converts all of them
+Code: romanNumeral("1 15 2000 1893")
+Expected Output: "I XV MM MDCCCXCIII"
 
-
-
-thousands - 1=m, 2=mm, 3=mmm can't be higher
-hundreds - 1=C, 2=CC, 3=CCC, 4=CD, 5=D, 6=DC, 7=DCC, 8=DCCC, 9=CM
-tens - 1=X, 2=XX, 3=XXX, 4=XL, 5=L, 6=LX, 7=LXX, 8=LXXX, 9=XC
-ones - 1=I, 2=II, 3=III, 4=IV, 5=V, 6=VI, 7=VII, 8=VIII, 9=IX
-
-input 56 output LVI
-5 and 6
-L     VI
-
-Symbol  Value
-I       1
-IV      4
-V       5
-IX      9
-X       10
-L       50
-C       100
-D       500
-M       1,000
+Test: The input ignores all punctuation
+Code: romanNumeral("2,021");
+Expected Output: "MMXXI"
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,67 @@
 function romanNumerals(number) {
   if (!parseInt(number)) {
-    return "Please input a number";
+    return "'NaN'";
   }
-  if (parseInt(number) > 3999 || parseInt(number) < 1) {
-    return "Please input a number between 1-3,999";
+  if (parseInt(number) > 3999) {
+    return "'Too Big'";
   }
-  const onesNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  const tensNumerals = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  const hundredsNumerals = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  const thousandsNumerals = ["M", "MM", "MMM"];
+  if (parseInt(number) < 1) {
+    return "'Too Small'";
+  }
+  const onesNumerals = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  const tensNumerals = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  const hundredsNumerals = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  const thousandsNumerals = ["", "M", "MM", "MMM"];
+  let output = [];
+  let input = number.split('');
+  if (input.length === 1) {
+    output.push(onesNumerals[input[0]]);
+  } else if (input.length === 2) {
+    output.push(tensNumerals[input[0]]);
+    output.push(onesNumerals[input[1]]);
+  } else if (input.length === 3) {
+    output.push(hundredsNumerals[input[0]]);
+    output.push(tensNumerals[input[1]]);
+    output.push(onesNumerals[input[2]]);
+  } else if (input.length === 4) {
+    output.push(thousandsNumerals[input[0]]);
+    output.push(hundredsNumerals[input[1]]);
+    output.push(tensNumerals[input[2]]);
+    output.push(onesNumerals[input[3]]);
+  }
 
-  
-  return onesNumerals[parseInt(number) - 1];
+  return output.join('');
 }
 
+//input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g,"");
+ /*
+take the input
+remove the punctuation
+split into array of multiple numbers
+array.foreach(function(element) {
+  print romanNumerals(element) + " "
+})
 
+print all the returns followed by spaces
+*/
 
+$(document).ready(function() {
+  $("#number").submit(function(event) {
+    event.preventDefault();
 
+    //sanitize the input of all excess punctuation and spaces
+    let input = $("#input").val().replace(/[.,\/#!$%?\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
+    let inputArr = input.split(' ');
+    let outputArr = [];
 
+    inputArr.forEach(function(element) {
+      outputArr.push(romanNumerals(element));
+    });
+    
+    $("ul").empty();
 
+    outputArr.forEach(function(number) {
+      $("ul").append("<li>" + number + "</li>");
+    });
+  });
+});
